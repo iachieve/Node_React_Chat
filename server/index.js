348fa6c31e4b5b617e4788aca3007960729b392f
iachieve <mhalim80@hotmail.com>
2020-04-05 07:44:52 -0700

    deployment

--- server/index.js
+++ server/index.js
@@ -10,11 +10,6 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// server react project
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-
 
 app.use(cors());
 
@@ -53,10 +48,13 @@ io.on('connect', (socket) => {
 });
 
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
